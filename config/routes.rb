1f899c585bc5706bd756d451747994015f115ae8
SyedMuhammadFaheem <seyd.faheem.official@gmail.com>
2023-06-12 15:12:21 +0500

    changes

--- config/routes.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get 'about', to:'about#index'
-  get '/', to:'main#index'
-  # Defines the root path route ("/")
-  # root "articles#index"
-end
