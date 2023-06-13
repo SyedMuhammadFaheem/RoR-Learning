3768b42e64b845be9cb1793ff1230c4f1d5a27a2
SyedMuhammadFaheem <syed.faheem.official@gmail.com>
2023-06-14 04:37:20 +0500

    conditionals covered

---
+++ basics/conditionals.rb
@@ -0,0 +1,22 @@
+num=2
+if num%2==0
+    puts 'number is even'
+else
+    puts 'number is odd'
+end
+
+
+# checking the largest integer
+a=10
+b=20
+c=30
+
+if a>b
+    if a>c
+        puts 'a is the largest'
+    end
+elsif b>c
+    puts 'b is the largest'
+else
+    puts 'c is the largest'
+end
